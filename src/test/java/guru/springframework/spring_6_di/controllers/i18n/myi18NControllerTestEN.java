831158d03db1cd9e41f22b52d7e3f2992e9898b0
zmelich <stocek86@gmail.com>
2025-10-21 15:32:41 +0200

    Practicing Spring profiles

---
+++ src/test/java/guru/springframework/spring_6_di/controllers/i18n/myi18NControllerTestEN.java
@@ -0,0 +1,25 @@
+package guru.springframework.spring_6_di.controllers.i18n;
+
+
+/*
+Created by Zsolt Melich (BT - IVR team)
+*/
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("EN")
+@SpringBootTest
+public class myi18NControllerTestEN {
+
+    @Autowired
+    Myi18NController myi18NController;
+
+    @Test
+    void sayHello(){
+        System.out.println(myi18NController.sayHello());
+    }
+
+}
