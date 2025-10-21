2725b18eb3ffa2164817903b135dba12aeeb7299
zmelich <stocek86@gmail.com>
2025-10-21 15:40:10 +0200

    Defining a default profile

--- src/main/java/guru/springframework/spring_6_di/services/i18n/EnglishGreetingService.java
+++ src/main/java/guru/springframework/spring_6_di/services/i18n/EnglishGreetingService.java
@@ -9,7 +9,7 @@ import guru.springframework.spring_6_di.services.GreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN","default"})
 @Service("i18NService")
 public class EnglishGreetingService implements GreetingService {
     @Override
--- src/test/java/guru/springframework/spring_6_di/controllers/i18n/myi18NControllerTestEN.java
+++ src/test/java/guru/springframework/spring_6_di/controllers/i18n/myi18NControllerTestEN.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("EN")
+//@ActiveProfiles("EN")
 @SpringBootTest
 public class myi18NControllerTestEN {
 
